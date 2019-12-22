4c7479cb6c889653ceae372e65df3f29893a5563
Cameron Albert <cameron@cameronalbert.com>
2019-12-22 14:24:57 -0800

    Initial creation.

---
+++ src/Lionsguard.Extensions.TypeHelpers/ReflectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace System
+{
+    public static class ReflectionExtensions
+    {
+        public static bool IsSubclassOrAssignableFrom<T>(this Type source)
+        {
+            return source.IsSubclassOrAssignableFrom(typeof(T));
+        }
+
+        public static bool IsSubclassOrAssignableFrom(this Type source, Type type)
+        {
+            var sourceInfo = source.GetTypeInfo();
+            var typeInfo = type.GetTypeInfo();
+
+            return sourceInfo.IsSubclassOf(type) || sourceInfo.IsAssignableFrom(typeInfo);
+        }
+    }
+}
---
+++ src/Lionsguard.Extensions.TypeHelpers/TypeHelper.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace System
+{
+    public static class TypeHelper
+    {
+        public static IEnumerable<Type> GetTypesWithCustomAttributes<TType, TAttribute>(bool isAbstract = false, bool isInterface = false)
+            where TAttribute : Attribute
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var types = new List<Type>();
+            foreach (var assembly in assemblies)
+            {
+                types.AddRange(assembly.GetTypes()
+                    .Where(t => t.IsAbstract == isAbstract
+                        && t.IsInterface == isInterface
+                        && (t.GetTypeInfo().IsSubclassOf(typeof(TType)) || typeof(TType).GetTypeInfo().IsAssignableFrom(t))
+                        && t.GetTypeInfo().GetCustomAttributes<TAttribute>().Any()
+                        ));
+            }
+            return types;
+        }
+
+        public static T ChangeType<T>(object value)
+        {
+            try
+            {
+                if (value == null)
+                    return default;
+
+                if (typeof(T).IsEnum)
+                {
+                    try
+                    {
+                        return (T)Enum.Parse(typeof(T), value.ToString());
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    return default;
+                }
+                if (typeof(T) == typeof(Guid))
+                {
+                    Guid.TryParse(value.ToString(), out var guid);
+                    return (T)(object)guid;
+                }
+                if (typeof(T) == typeof(DateTime))
+                {
+                    if (long.TryParse(value.ToString(), out var ticks))
+                    {
+                        return (T)(object)new DateTime(ticks);
+                    }
+                    else
+                    {
+                        DateTime.TryParse(value.ToString(), out var dt);
+                        return (T)(object)dt;
+                    }
+                }
+
+                if (value != null && typeof(T).IsSubclassOrAssignableFrom(value.GetType()))
+                    return (T)value;
+
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (InvalidCastException)
+            {
+                return default;
+            }
+        }
+    }
+}
